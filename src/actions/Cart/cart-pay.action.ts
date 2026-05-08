024f429a963ad05bf994f8749a7dc48be28519be
Khaled-AbHe <6320936@cegepmv.ca>
2026-05-07 23:47:03 -0400

    added cart page, created loader and actions that are used with useFetcher

---
+++ src/actions/Cart/cart-pay.action.ts
@@ -0,0 +1,25 @@
+import { payCartTotal } from "../../services/api";
+import type ActionReturnMessage from "../../interfaces/action-return.interface";
+
+export async function cartPayAction({ request }: any): Promise<ActionReturnMessage> {
+  const formData = await request.formData();
+
+  const method = formData.get("method") as string;
+  const amount = parseFloat(formData.get("amount") as string);
+
+  console.log(`${amount}`);
+
+  if (!method || isNaN(amount)) {
+    return { type: "error", message: "Invalid payment details." };
+  }
+
+  try {
+    await payCartTotal({ method, amount });
+    return { type: "success", message: "Payment received!" };
+  } catch (error: any) {
+    return {
+      type: "error",
+      message: error.message ?? "Payment failed. Please try again.",
+    };
+  }
+}
