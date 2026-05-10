21fe57529f06b4320dbc4dc7776dd40ca26641b8
amineelghazi100 <amineelghazi100@hotmail.com>
2026-05-09 21:46:58 -0400

    Fixed merge conflit

---
+++ src/components/Vehicle/VehiclesType.ts
@@ -0,0 +1,3 @@
+export type VehicleType = 'truck' | 'trailer'
+export type TruckType = 'Pickup' |'Cargo Van' | 'Small Box' | 'Medium Box' | 'Large Box' | 'XLarge Box';
+export type TrailerType = 'Small' | 'Medium' | 'Large';
