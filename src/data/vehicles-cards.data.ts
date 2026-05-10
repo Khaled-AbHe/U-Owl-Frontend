722238736d28a27b2a63e623294b4061dcd532e0
amineelghazi100 <amineelghazi100@hotmail.com>
2026-05-10 02:42:43 -0400

    Changed the entire vehicles page, structure, removed filters & showed only raw data from vehicles-tables.data

---
+++ src/data/vehicles-cards.data.ts
@@ -0,0 +1,126 @@
+export let Title = "U-Owl Moving Truck Rentals";
+export let ShowingCompanyDescription =
+  "U-Owl is your go-to solution for all your moving needs. As the leading moving and storage resource, U-Haul has the largest selection of truck rentals for your upcoming move.";
+
+export const trucksCards = [
+  {
+    img: "img.png",
+    type: "Pickup Truck",
+    desc: "Small Jobs",
+  },
+  {
+    img: "img.png",
+    type: "Cargo Van",
+    desc: "Small Jobs",
+  },
+  {
+    img: "img.png",
+    type: "10'Truck",
+    desc: "Studio / 1 Bedroom Apt",
+  },
+  {
+    img: "img.png",
+    type: "15'Truck",
+    desc: "1 - 2 Bedroom(s) Home/Apartment",
+  },
+  {
+    img: "img.png",
+    type: "20'Truck",
+    desc: "2 - 3 Bedroom(s) Home/Apartment",
+  },
+  {
+    img: "img.png",
+    type: "26'Truck",
+    desc: "3 - 4 Bedroom Home/Apartment",
+  },
+];
+
+export const trucksStorageCards = [
+  {
+    storage: "5' x 10' x 8'",
+  },
+  {
+    storage: "5' x 10' x 8'",
+  },
+  {
+    storage: "5' x 10' x 8'",
+  },
+  {
+    storage: "10' x 10' x 8''",
+  },
+  {
+    storage: "10' x 15' x 8'",
+  },
+  {
+    storage: "10' x 20' x 8'",
+  },
+];
+
+export const dimensionsCards= [
+  //Pickup Truck
+  {
+    equivalentTruckSizes: "6', 7', 8'",
+    insideDimensions: "7'10' x 5'2' x 1'9'",
+    deckHeightFromGround: '34"',
+    deckLength: '7"10"',
+    volume: "76 cu. ft.",
+    maxLoad: "2,280 lbs.",
+    grossVehicleWeight: "6,800 lbs. max",
+    emptyWeight: "4,474 lbs.",
+  },
+  //Cargo Van
+  {
+    equivalentTruckSizes: '7", 8" 9"',
+    insideDimensions: '9"6" x 5"7" x 4"8"',
+    deckHeightFromGround: '29"',
+    deckLength: '9"6"',
+    volume: "246 cu. ft.",
+    maxLoad: "4,030 lbs.",
+    grossVehicleWeight: "9,070 lbs. max",
+    emptyWeight: "4,985 lbs.",
+  },
+  //10' Truck
+  {
+    equivalentTruckSizes: '9", 10"',
+    insideDimensions: '9"11" x 6"3" x 6"1"',
+    deckHeightFromGround: '29"',
+    deckLength: '9"11"',
+    volume: "402 cu. ft.",
+    maxLoad: "2,850 lbs.",
+    grossVehicleWeight: "8,600 lbs. max",
+    emptyWeight: "5,790 lbs.",
+  },
+  //15' Truck
+  {
+    equivalentTruckSizes: '11", 12", 13", 14", 15"',
+    insideDimensions: '15" x 7"8" x 7"2"',
+    deckHeightFromGround: '33"',
+    deckLength: '12"5"',
+    volume: "764 cu. ft.",
+    maxLoad: "6,385 lbs.",
+    grossVehicleWeight: "14,500 lbs. max",
+    emptyWeight: "8,115 lbs.",
+  },
+  //20' Truck
+  {
+    equivalentTruckSizes: '16", 17", 18", 19", 20"	',
+    insideDimensions: '19"6" x 7"8" x 7"2"',
+    deckHeightFromGround: '35"',
+    deckLength: '16"10"',
+    volume: "1,016 cu. ft.	",
+    maxLoad: "5,700 lbs.",
+    grossVehicleWeight: "14,500 lbs. max	",
+    emptyWeight: "8,800 lbs.",
+  },
+  //26' Truck
+  {
+    equivalentTruckSizes: '21", 22", 23", 24", 25", 26"',
+    insideDimensions: '26"2" x 8"2" x 8"3"',
+    deckHeightFromGround: '35"',
+    deckLength: '23"5"',
+    volume: "1,682 cu. ft.",
+    maxLoad: "12,859 lbs.",
+    grossVehicleWeight: "25,999 lbs. max",
+    emptyWeight: "13,140 lbs.",
+  },
+];
